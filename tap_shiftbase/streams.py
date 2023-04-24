fd54efe88f4dc13601d797e61b851440672cd869
Hidde Stokvis <hidde@trunkrs.nl>
2023-04-24 08:55:52 +0200

    Fix: all primary keys

--- tap_shiftbase/streams.py
+++ tap_shiftbase/streams.py
@@ -13,7 +13,7 @@ SCHEMAS_DIR = Path(__file__).parent / Path("./schemas")
 class UsersStream(shiftbaseStream):
     name = "users"
     path = "/users"
-    primary_keys = ["Users.id"]
+    primary_keys = ["user__id"]
     replication_key = None
     schema_filepath = SCHEMAS_DIR / "users.json"
 
@@ -27,62 +27,62 @@ class AbsenteeOptionsStream(shiftbaseStream):
 class AbsenteesStream(shiftbaseStream):
     name = "absentees"
     path = "/absentees"
-    primary_keys = ["id"]
+    primary_keys = ["absentee__id"]
     replication_key = None
     schema_filepath = SCHEMAS_DIR / "absentees.json"
 
 class ContractTypesStream(shiftbaseStream):
     name = "contract_types"
     path = "/contract_types"
-    primary_keys = ["id"]
+    primary_keys = ["contracttype__id"]
     replication_key = None
     schema_filepath = SCHEMAS_DIR / "contract_types.json"
 
 class CustomFieldsStream(shiftbaseStream):
     name = "custom_fields"
     path = "/custom_fields"
-    primary_keys = ["id"]
+    primary_keys = ["customfield__id"]
     replication_key = None
     schema_filepath = SCHEMAS_DIR / "custom_fields.json"
 
 class DepartmentsStream(shiftbaseStream):
     name = "departments"
     path = "/departments"
-    primary_keys = ["id"]
+    primary_keys = ["department__id"]
     replication_key = None
     schema_filepath = SCHEMAS_DIR / "departments.json"
 
 class LocationsStream(shiftbaseStream):
     name = "locations"
     path = "/locations"
-    primary_keys = ["id"]
+    primary_keys = ["location__id"]
     replication_key = None
     schema_filepath = SCHEMAS_DIR / "locations.json"
 
 class RostersStream(shiftbaseStream):
     name = "rosters"
     path = "/rosters"
-    primary_keys = ["id"]
+    primary_keys = ["roster__id"]
     replication_key = None
     schema_filepath = SCHEMAS_DIR / "rosters.json"
 
 class ShiftsStream(shiftbaseStream):
     name = "shifts"
     path = "/shifts"
-    primary_keys = ["id"]
+    primary_keys = ["shift__id"]
     replication_key = None
     schema_filepath = SCHEMAS_DIR / "shifts.json"
 
 class TeamsStream(shiftbaseStream):
     name = "teams"
     path = "/teams"
-    primary_keys = ["id"]
+    primary_keys = ["team__id"]
     replication_key = None
     schema_filepath = SCHEMAS_DIR / "teams.json"
 
 class TimesheetsStream(shiftbaseStream):
     name = "timesheets"
     path = "/timesheets"
-    primary_keys = ["id"]
+    primary_keys = ["timesheet__id"]
     replication_key = None
     schema_filepath = SCHEMAS_DIR / "timesheets.json"
